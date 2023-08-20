288aa6806d1b720c4778d2212d8410a90db82628
Maurycy J <maurycy.kzmj@gmail.com>
2023-08-20 13:24:42 +0200

    larger kernel sizes

--- src/program/val_program.rs
+++ src/program/val_program.rs
@@ -1,4 +1,6 @@
-use glium::{program::ComputeShader, uniform, Display, Texture2d};
+use std::mem::size_of;
+
+use glium::{program::ComputeShader, uniform, Display, Texture2d, uniforms::UniformBuffer};
 use yaml_rust::Yaml;
 
 use super::{Program, EdgeSolution};
@@ -8,7 +10,8 @@ pub struct ValProgram {
     height: u32,
     convolution_shader: ComputeShader,
     swap_shader: ComputeShader,
-    kernel: [[f32; 3]; 3],
+    kernel_buf: UniformBuffer<[f32]>,
+    kernel_size: usize
 }
 
 impl ValProgram {
@@ -16,33 +19,38 @@ impl ValProgram {
         width: u32,
         height: u32,
         fun: &str,
-        kernel: &Vec<Vec<f32>>,
+        kernel: Vec<Vec<f32>>,
         display: &Display,
         edge_solution: EdgeSolution,
         edge_val: Option<f32>
     ) -> Self {
-        let mut kernel_arr: [[f32; 3]; 3] = [[0.; 3]; 3];
-        for (y, row) in kernel.into_iter().enumerate() {
-            for (x, v) in row.into_iter().enumerate() {
-                kernel_arr[y][x] = *v;
-            }
-        }
         let clamp_src = match edge_solution {
             EdgeSolution::Clamp => CSAMPLE_CLAMP_SRC.to_string(),
             EdgeSolution::Wrap => CSAMPLE_WRAP_SRC.to_string(),
             EdgeSolution::Value => csample_val_src(edge_val.unwrap()),
         };
+        let kernel_size = kernel.len();
+        let flat_kernel: Vec<f32> = kernel.iter()
+            .flatten()
+            .copied()
+            .collect();
+
+        
+        let kernel_buf: UniformBuffer<[f32]> = UniformBuffer::empty_unsized_immutable(display, kernel_size*kernel_size*size_of::<f32>()).unwrap();
+        kernel_buf.write(&flat_kernel);
+
         Self {
             width,
             height,
             convolution_shader: glium::program::ComputeShader::from_source(
                 display,
-                &convolution_shader_src(fun, &clamp_src)
+                &convolution_shader_src(fun, &clamp_src, kernel_size * kernel_size)
             )
             .unwrap(),
             swap_shader: glium::program::ComputeShader::from_source(display, &SWAP_SHADER_SRC)
                 .unwrap(),
-            kernel: kernel_arr,
+            kernel_buf,
+            kernel_size
         }
     }
 }
@@ -53,9 +61,15 @@ impl Program for ValProgram {
             .image_unit(glium::uniforms::ImageUnitFormat::RGBA32F)
             .unwrap()
             .set_access(glium::uniforms::ImageUnitAccess::ReadWrite);
-
+        
         self.convolution_shader.execute(
-            uniform! { uWidth: self.width, uHeight: self.height, uKernelSize: 3, uKernel: self.kernel, uTexture: image_unit}, 
+            uniform! { 
+                uWidth: self.width, 
+                uHeight: self.height, 
+                uKernelSize: self.kernel_size as i32, 
+                uKernel: &self.kernel_buf, 
+                uTexture: image_unit
+            }, 
             self.width/16 + if self.width % 16 > 0 {1} else {0}, 
             self.height/16 + if self.height % 16 > 0 {1} else {0}, 
             1
@@ -99,7 +113,7 @@ impl Program for ValProgram {
             doc["screen"][0].as_i64().unwrap().try_into().unwrap(),
             doc["screen"][1].as_i64().unwrap().try_into().unwrap(),
             doc["fun"].as_str().unwrap(),
-            &doc["kernel"]
+            doc["kernel"]
                 .as_vec()
                 .unwrap()
                 .into_iter()
@@ -118,7 +132,7 @@ impl Program for ValProgram {
                         })
                         .collect::<Vec<f32>>()
                 })
-                .collect(),
+                .collect::<Vec<Vec<f32>>>(),
             display,
             edge,
             val
@@ -146,7 +160,7 @@ void main() {
 }
 "#;
 
-fn convolution_shader_src(fun_src: &str, csample_src: &str) -> String {
+fn convolution_shader_src(fun_src: &str, csample_src: &str, kernel_size_sq: usize) -> String {
     format!(
         "#version 430
 
@@ -155,32 +169,31 @@ fn convolution_shader_src(fun_src: &str, csample_src: &str) -> String {
     uniform uint uWidth;
     uniform uint uHeight;
     uniform int uKernelSize;
-    uniform mat3 uKernel;
+    uniform uKernel{{
+        float kernel[{kernel_size_sq}];
+    }};
     uniform layout(binding=3, rgba32f) image2D uTexture;
 
     vec4 csample(ivec2 i) {{
-        {}
+        {csample_src}
     }}
     float fun(float x, float prev) {{
-        {}
+        {fun_src}
     }}
 
     void main() {{
         ivec2 i = ivec2(gl_GlobalInvocationID.x, gl_GlobalInvocationID.y);
-        int kernelSize = uKernelSize/2;
         if (i.x >= int(uWidth) || i.y >= int(uHeight))
             return;
 
         float sum = 0;
-        for (int y = -kernelSize; y <= kernelSize; ++y) 
-        for (int x = -kernelSize; x <= kernelSize; ++x) 
-            sum += csample(i + ivec2(x,y)).r * uKernel[y + kernelSize][x + kernelSize];
+        int offset = uKernelSize / 2;
+        for (int k = 0; k < uKernelSize*uKernelSize; ++k)
+            sum += csample(i + ivec2(mod(k, uKernelSize) - offset, k / uKernelSize - offset)).r * kernel[k];
 
         vec4 pixel_sample = imageLoad(uTexture, i);
         imageStore(uTexture, i, vec4(pixel_sample.r, fun(sum, pixel_sample.r), pixel_sample.b, pixel_sample.a) );
-    }}",
-        csample_src, fun_src
-    )
+    }}" )
 }
 
 
